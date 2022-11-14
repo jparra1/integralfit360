f786fb783d148d2dbc3d570d938093fe926d5c34
Edison Parra <j.parra1@utp.edu.co>
2022-11-13 22:17:08 -0500

    Ajustes en home page y conexion a bd servidor

--- php/BD_info.php
+++ php/BD_info.php
@@ -1,8 +1,13 @@
 <?php
-    $pdo=null;
-    $host='localhost';
-    $user='root';
-    $password='123456';
-    $bd='integral_360';
+    // $pdo=null;
+    // $host='localhost';
+    // $user='root';
+    // $password='123456';
+    // $bd='integral_360';
+
+    $host='db5010828440.hosting-data.io';
+    $user='dbu2746813';
+    $password='Integral.360-2022';
+    $bd='dbs9160260';
 
 ?>
