d269f6695c9121445464293eb17c013f6356a004
Saquial <samuel.quintero1@utp.edu.co>
2022-11-19 13:55:45 -0500

    Cambio planes homepage
    La modal de planes v2 ha sido implementada en la homepage, pero falta implementar el enrutamiento en el botón confirmar.

--- js/payments.js
+++ js/payments.js
@@ -1,5 +1,9 @@
 
 function mediopago() {
+    var pEle = document.getElementById("pEl").textContent;
+    var pDur = document.getElementById("pDu").textContent;
+    var pCos = document.getElementById("pCo").textContent;
+
     var card = document.getElementById("itarjeta").value;
     var date = document.getElementById("ifecha").value;
     var secCode = document.getElementById("icodigoseguridad").value;
@@ -11,48 +15,58 @@ function mediopago() {
     var postal = document.getElementById("ipostal").value;
     var phone = document.getElementById("itelefono").value;
 
-    if (card != "" && date != "" && secCode != "" && name != "" && surname != "" && city != "" && country != "" && address != "" && postal != "" && phone != "") {
-        $.ajax({
-            url: '../php/points/pointPayment.php',//ubicacion del ponit
-            method: 'POST',// metodo de envio
-            responseType: 'json',//formato recibido
-            data: {
-                tarjeta: card, fecha: date, codseg: secCode, nombre: name, apellido: surname,
-                ciudad: city, pais: country, direccion: address, codpostal: postal, telefono: phone
-            },//informacion que envio en json
-            async: false//hasta que termine la consulta no ejecuta el then
-        }).then(function (data) {
-            var div_log = document.getElementById("log_ingreso")
-            try {
-                var dataRecive = JSON.parse(data);
-                if (true) { //variable placeholder, no se cual es la condición de éxito
-                    Swal.fire({
-                        icon: 'success',
-                        title: '',
-                        confirmButtonText: "Entendido",
-                        text: '',
-                    });
+    if (pEle != "" && pDur != "" && pCos != "") {
+        if (card != "" && date != "" && secCode != "" && name != "" && surname != "" && city != "" && country != "" && address != "" && postal != "" && phone != "") {
+            $.ajax({
+                url: '../php/points/pointPayment.php',//ubicacion del ponit
+                method: 'POST',// metodo de envio
+                responseType: 'json',//formato recibido
+                data: {
+                    tarjeta: card, fecha: date, codseg: secCode, nombre: name, apellido: surname,
+                    ciudad: city, pais: country, direccion: address, codpostal: postal, telefono: phone
+                },//informacion que envio en json
+                async: false//hasta que termine la consulta no ejecuta el then
+            }).then(function (data) {
+                var div_log = document.getElementById("log_ingreso")
+                try {
+                    var dataRecive = JSON.parse(data);
+                    if (true) { //variable placeholder, no se cual es la condición de éxito
+                        Swal.fire({
+                            icon: 'success',
+                            title: '',
+                            confirmButtonText: "Entendido",
+                            text: '',
+                        });
 
-                    div_log.innerHTML += `<label class="col-10" style="color: white;"> --> Ingreso Exitoso de - ${email} : estatus: ${dataRecive["estatus"]} - mensaje: ${dataRecive["Mensaje"]} - Nombre: ${dataRecive["data_usuario"]["usuario"]}</label><br>
+                        div_log.innerHTML += `<label class="col-10" style="color: white;"> --> Ingreso Exitoso de - ${email} : estatus: ${dataRecive["estatus"]} - mensaje: ${dataRecive["Mensaje"]} - Nombre: ${dataRecive["data_usuario"]["usuario"]}</label><br>
                           `;
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Hubo un error',
-                        text: 'La información dada no es correcta',
-                    });
-                    div_log.innerHTML += `<labelclass="col-10" style="color: red;"> --> Ingreso Fallido de - ${email} : Estatus ${dataRecive["estatus"]} - Mensaje ${dataRecive["Mensaje"]} </label><br>
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Hubo un error',
+                            text: 'La información dada no es correcta',
+                        });
+                        div_log.innerHTML += `<labelclass="col-10" style="color: red;"> --> Ingreso Fallido de - ${email} : Estatus ${dataRecive["estatus"]} - Mensaje ${dataRecive["Mensaje"]} </label><br>
                           `;
+                    }
+
+                } catch (error) {
+                    console.log(data);
+                    div_log.innerHTML += `<labelclass="col-10" style="color: red;"> ***** ERROR DE LOGICA 500 </label><br>`;
                 }
+            });
 
-            } catch (error) {
-                console.log(data);
-                div_log.innerHTML += `<labelclass="col-10" style="color: red;"> ***** ERROR DE LOGICA 500 </label><br>`;
-            }
+
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Plan no elegido',
+            text: 'Por favor seleccione algún plan para continuar',
         });
-
-
     }
+
+    
 }
 
 function selectS(button, price, duration) {
@@ -64,7 +78,7 @@ function selectS(button, price, duration) {
     document.getElementById("pSport").textContent = price;
 
     button.className = "btn btn-info button3"
-    button.style = "width: auto; background-color: #012626; font-weight: bold; font-size: 10px;"
+    button.style = "background-color: #012626; font-weight: bold; font-size: 10px;  width: 70px"
 }
 
 function selectH(button, price, duration) {
@@ -76,7 +90,7 @@ function selectH(button, price, duration) {
     document.getElementById("pHealth").textContent = price;
 
     button.className = "btn btn-info button3"
-    button.style = "width: auto; background-color: #012626; font-weight: bold; font-size: 10px;"
+    button.style = "background-color: #012626; font-weight: bold; font-size: 10px;  width: 70px"
 }
 
 function selectC(button, price, duration) {
@@ -88,7 +102,7 @@ function selectC(button, price, duration) {
     document.getElementById("pComplete").textContent = price;
 
     button.className = "btn btn-info button3"
-    button.style = "width: auto; background-color: #012626; font-weight: bold; font-size: 10px;"
+    button.style = "background-color: #012626; font-weight: bold; font-size: 10px;  width: 70px"
 }
 
 function changeR() {
@@ -103,24 +117,24 @@ function limpiar() {
     document.getElementById("pComplete").textContent = "";
 
     document.getElementById("bs1").className = "btn btn-info button2"
-    document.getElementById("bs1").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bs1").style = "font-weight: bold; font-size: 10px; width: 70px"
     document.getElementById("bs2").className = "btn btn-info button2"
-    document.getElementById("bs2").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bs2").style = "font-weight: bold; font-size: 10px; width: 70px"
     document.getElementById("bs3").className = "btn btn-info button2"
-    document.getElementById("bs3").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bs3").style = "font-weight: bold; font-size: 10px; width: 70px"
 
     document.getElementById("bh1").className = "btn btn-info button2"
-    document.getElementById("bh1").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bh1").style = "font-weight: bold; font-size: 10px; width: 70px"
     document.getElementById("bh2").className = "btn btn-info button2"
-    document.getElementById("bh2").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bh2").style = "font-weight: bold; font-size: 10px; width: 70px"
     document.getElementById("bh3").className = "btn btn-info button2"
-    document.getElementById("bh3").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bh3").style = "font-weight: bold; font-size: 10px; width: 70px"
 
     document.getElementById("bc1").className = "btn btn-info button2"
-    document.getElementById("bc1").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bc1").style = "font-weight: bold; font-size: 10px; width: 70px"
     document.getElementById("bc2").className = "btn btn-info button2"
-    document.getElementById("bc2").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bc2").style = "font-weight: bold; font-size: 10px; width: 70px"
     document.getElementById("bc3").className = "btn btn-info button2"
-    document.getElementById("bc3").style = "font-weight: bold; font-size: 10px;"
+    document.getElementById("bc3").style = "font-weight: bold; font-size: 10px; width: 70px"
 
 }
